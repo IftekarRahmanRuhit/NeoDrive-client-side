1e461e39f7cd5e78d0cb550322a0e46c17f27250
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-12-21 20:54:21 +0600

    initial setup complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import {
+    createBrowserRouter,
+
+  } from "react-router-dom";
+
+
+export const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+
+
+
+  ]);
